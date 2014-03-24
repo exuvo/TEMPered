b58e453c5432b3a6da534a208e3f81fbdd4ebbe6
exuvo <exuvo@exuvo.se>
2014-03-24 23:26:29 +0100

    .

--- bygg.sh
+++ bygg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get -y install binutils gcc make cmake autoconf
+sudo apt-get -y install binutils gcc make cmake autoconf libtool
 DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 make
