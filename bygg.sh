bfdd9a62bb1f1a4279e027a1f295b998c62ab6ce
exuvo <exuvo@exuvo.se>
2014-03-24 23:17:59 +0100

    .

--- bygg.sh
+++ bygg.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-sudo apt-get -y install binutils gcc make cmake
+#sudo apt-get -y install binutils gcc make cmake
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
 make
 cp tempered ../
